b26973b1e315049c316ee423ddbcab85854c50ac
Lukesudom <lukemarshall511@gmail.com>
2022-08-01 20:29:08 +1000

    troubleshooting heroku deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const port = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
